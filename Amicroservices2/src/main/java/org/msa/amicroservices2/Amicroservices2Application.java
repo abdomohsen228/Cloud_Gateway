76f57ffdc997ee6769cd2da7d3151588fe567494
Abdelrhman <abdo.mohsen3457643@gmail.com>
2024-12-04 21:36:46 +0200

    CircuitBreaker pattern , fault tolerance ,  defacing in code

--- Amicroservices2/src/main/java/org/msa/amicroservices2/Amicroservices2Application.java
+++ Amicroservices2/src/main/java/org/msa/amicroservices2/Amicroservices2Application.java
@@ -1,10 +1,10 @@
 package org.msa.amicroservices2;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -18,16 +18,22 @@ public class Amicroservices2Application {
 
     @Bean
     public RestTemplate restTemplate() {
-       return new RestTemplate();
+        return new RestTemplate();
     }
+
     @RestController
     class ConsumerController {
         @Autowired
         private RestTemplate restTemplate;
         @GetMapping("api/message")
+//         prevent cascading failures in a microservices architecture by providing fallback methods when a service or component becomes unavailable.
+        @CircuitBreaker(name = "msa1", fallbackMethod = "getDefaultMessage")
         public String getMessageFromMs1() {
-            final String message = restTemplate.getForObject("http://localhost:9091/api/test/message", String.class);
-            return message;
+            return restTemplate.getForObject("http://localhost:9091/api/test/message", String.class);
+        }
+
+        public String getDefaultMessage(Throwable throwable) {
+            return "This is a default message as the service is unavailable.";
         }
     }
 }
